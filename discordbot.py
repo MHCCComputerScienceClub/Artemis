d34a07df8c99e5162163a3836da02be902a7504c
Ryan Mattox <509528@gmail.com>
2018-05-13 23:38:42 -0700

    Added good bot bad bot and code formatting

--- discordbot.py
+++ discordbot.py
@@ -6,6 +6,11 @@ import config
 
 name = "artemis"
 
+CODEWEIGHT = 0.21
+PLAINWEIGHT = 0.03191 
+PLCDRATIO = (PLAINWEIGHT + CODEWEIGHT) / 2 # Plain to code ratio
+SPECIALSET = "[]{};:\"/<>|\\-_=+#$%^&*()@~`"
+
 print(name + ' is starting up...')
 
 client = discord.Client()
@@ -31,6 +36,12 @@ async def on_message(message):
         await client.send_message(message.channel, "I don't have emotions. I'm a robot.")
         time.sleep(0.5)
         await client.send_message(message.channel, "How are you, " + message.author.mention + "?")
+
+    elif message.content.lower().startswith("good bot"):
+        await client.send_message(message.channel, "Thank you, " + message.author.mention)
+
+    elif message.content.lower().startswith("bad bot"):
+        await client.send_message(message.channel, "I'm sorry, I'll try to do better next time :frowning:")
     
     # Dice rolling script
     elif message.content.startswith("!roll"):
@@ -78,5 +89,36 @@ async def on_message(message):
                     "Outlook not so good.",
                     "Very doubtful."]
         await client.send_message(message.channel, response[roll])
+
+    # Automatic code formatter - Ryan
+    elif ('`' not in message.content) or (len(message.content) < 10):
+
+        # Create a count for each character 
+        charCount = {}
+        for char in message.content:
+            if(char in charCount):
+                charCount[char] += 1
+            else:
+                charCount[char] = 1
+        specialCount = 0
+
+        # Add up the number of special character occurances
+        for char in SPECIALSET:
+            if(char in charCount):
+                specialCount += charCount[char]
+
+        # Generate a ratio of special to nonspecial
+        ratio = specialCount/(len(message.content)*1.0)
+
+        if ratio > PLCDRATIO:
+            await client.send_message(message.channel, "Excuse me, good sir. I "
+                    "believe you forgot to format your code. I'll do it for you "
+                    "this time, but next time remember to begin and end your code with "
+                    "'\`\`\`' for multiple lines of code, or a single '\`' for inline "
+                    "code.\n ```" + message.content +
+                    '```\n\nRatio of special to non-special: ' +
+                    str(round(ratio*100, 2)) +  # Ratio of special characters to nonspecial ones
+                    ' | Confidence: ' + 
+                    str(round((ratio/(CODEWEIGHT - PLAINWEIGHT)) * 100, 2)) + '%') # Confidence level
 
 client.run(config.token)
